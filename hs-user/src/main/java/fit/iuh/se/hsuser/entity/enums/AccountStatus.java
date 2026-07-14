e795c3103093bbd0684643616b9599b68377a477
phatdang <phatdang19052004@gmail.com>
2026-07-14 17:07:43 +0700

    feat(entity.enums): add AccountStatus and UserRole enums for user management

---
+++ hs-user/src/main/java/fit/iuh/se/hsuser/entity/enums/AccountStatus.java
@@ -0,0 +1,6 @@
+package fit.iuh.se.hsuser.entity.enums;
+
+public enum AccountStatus {
+    ACTIVE,
+    INACTIVE
+}
---
+++ hs-user/src/main/java/fit/iuh/se/hsuser/entity/enums/UserRole.java
@@ -0,0 +1,7 @@
+package fit.iuh.se.hsuser.entity.enums;
+
+public enum UserRole {
+    DOCTOR,
+    MEMBER,
+    ADMIN
+}
