5eee75a1a7fe26280ac0dd427eeb8b31bd1b99a9
phatdang <phatdang19052004@gmail.com>
2026-07-14 17:08:00 +0700

    feat(application): enable JPA repositories and auto-configuration for application package

--- hs-application/src/main/java/fit/iuh/se/hsapplication/HsApplication.java
+++ hs-application/src/main/java/fit/iuh/se/hsapplication/HsApplication.java
@@ -1,9 +1,13 @@
 package fit.iuh.se.hsapplication;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "fit.iuh.se")
+@AutoConfigurationPackage(basePackages = "fit.iuh.se")
+@EnableJpaRepositories(basePackages = "fit.iuh.se")
 public class HsApplication {
 
     public static void main(String[] args) {
