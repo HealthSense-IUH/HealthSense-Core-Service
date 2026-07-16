7b92dd6d4c94b844594f66d2291ceef179730f77
phatdang <phatdang19052004@gmail.com>
2026-07-16 08:47:21 +0700

    feat(controller): add AuthController with registration endpoint

---
+++ hs-auth/src/main/java/fit/iuh/se/hsauth/controller/AuthController.java
@@ -0,0 +1,28 @@
+package fit.iuh.se.hsauth.controller;
+
+import fit.iuh.se.hsauth.dto.request.RegisterRequest;
+import fit.iuh.se.hsauth.dto.response.UserSessionResponse;
+import fit.iuh.se.hsauth.service.AuthService;
+import fit.iuh.se.hsshared.dto.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthController {
+
+    AuthService authService;
+
+    @PostMapping("/register")
+    public ApiResponse<UserSessionResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return new ApiResponse<>(authService.register(request));
+    }
+}
