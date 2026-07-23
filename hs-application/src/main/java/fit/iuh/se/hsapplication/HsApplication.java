78167da6fbbca3237bed8a712abdf238893e620d
phatdang <phatdang19052004@gmail.com>
2026-07-23 19:20:59 +0700

    feat(application): enable JPA auditing for enhanced entity tracking

--- hs-application/src/main/java/fit/iuh/se/hsapplication/HsApplication.java
+++ hs-application/src/main/java/fit/iuh/se/hsapplication/HsApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "fit.iuh.se")
 @AutoConfigurationPackage(basePackages = "fit.iuh.se")
 @EntityScan(basePackages = "fit.iuh.se")
 @EnableJpaRepositories(basePackages = "fit.iuh.se")
+@EnableJpaAuditing
 public class HsApplication {
 
     public static void main(String[] args) {
