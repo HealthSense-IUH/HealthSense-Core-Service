ff808bf4bf6c48399c48f2991f1297e170285b23
NgocHung11 <nhpel11@gmail.com>
2026-07-26 18:33:31 +0700

    fix(user): update validation logic to allow admins to update profiles

--- hs-user/src/main/java/fit/iuh/se/hsuser/service/user/impl/UserServiceImpl.java
+++ hs-user/src/main/java/fit/iuh/se/hsuser/service/user/impl/UserServiceImpl.java
@@ -110,9 +110,9 @@ public class UserServiceImpl implements UserService {
     }
 
     private void validateSelfUpdateRole(UserRole currentUserRole) {
-        if (currentUserRole == UserRole.DOCTOR || currentUserRole == UserRole.MEMBER)
+        if (currentUserRole == UserRole.ADMIN || currentUserRole == UserRole.DOCTOR || currentUserRole == UserRole.MEMBER)
             return;
-        throw new AppException(ErrorCode.ACCESS_DENIED, "Only doctors and members can update profile here");
+        throw new AppException(ErrorCode.ACCESS_DENIED, "Only admins, doctors and members can update profile here");
     }
 
     private void updateProfile(UserProfile profile, UserProfileUpdateRequest request) {
