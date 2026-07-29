f882a94a6c710e9c2ace839a41941eb37aa74a2c
NgocHung11 <nhpel11@gmail.com>
2026-07-29 14:02:36 +0700

    refactor: remove deprecated legacy authentication module

--- hs-notification/src/main/java/fit/iuh/se/hsnotification/config/AsyncConfig.java
+++
@@ -1,9 +0,0 @@
-package fit.iuh.se.hsnotification.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@Configuration
-@EnableAsync
-public class AsyncConfig {
-}
