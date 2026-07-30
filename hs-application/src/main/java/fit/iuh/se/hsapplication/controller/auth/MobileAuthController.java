ebbb921ccb1228b8c2ec90d3beb73d773b5524d6
phatdang <phatdang19052004@gmail.com>
2026-07-30 10:58:22 +0700

    refactor(controller): reorganize authentication and health record controllers into separate packages

--- hs-application/src/main/java/fit/iuh/se/hsapplication/controller/auth/MobileAuthController.java
+++ hs-application/src/main/java/fit/iuh/se/hsapplication/controller/auth/MobileAuthController.java
@@ -1,4 +1,4 @@
-package fit.iuh.se.hsapplication.controller;
+package fit.iuh.se.hsapplication.controller.auth;
 
 import fit.iuh.se.hsauth.dto.request.LoginRequest;
 import fit.iuh.se.hsauth.dto.request.MobileLogoutRequest;
