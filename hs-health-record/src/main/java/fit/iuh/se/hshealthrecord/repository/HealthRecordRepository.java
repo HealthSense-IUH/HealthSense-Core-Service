640c88d451e8b62434ae653e29009b9347425fd1
phatdang <phatdang19052004@gmail.com>
2026-08-08 16:51:43 +0700

    feat(HealthRecordRepository): extend repository to support JPA specifications

--- hs-health-record/src/main/java/fit/iuh/se/hshealthrecord/repository/HealthRecordRepository.java
+++ hs-health-record/src/main/java/fit/iuh/se/hshealthrecord/repository/HealthRecordRepository.java
@@ -4,12 +4,13 @@ import fit.iuh.se.hshealthrecord.entity.HealthRecord;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface HealthRecordRepository extends JpaRepository<HealthRecord, Long> {
+public interface HealthRecordRepository extends JpaRepository<HealthRecord, Long>, JpaSpecificationExecutor<HealthRecord> {
 
     Page<HealthRecord> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
 
