5533c047764d95f43767f278ae0949099e728082
phatdang <phatdang19052004@gmail.com>
2026-08-10 00:12:47 +0700

    feat(mapper): add mappings for CareServicePackage and DoctorCareProfile responses

--- hs-chat/src/main/java/fit/iuh/se/hschat/mapper/ConsultationMapper.java
+++ hs-chat/src/main/java/fit/iuh/se/hschat/mapper/ConsultationMapper.java
@@ -4,10 +4,14 @@ import fit.iuh.se.hschat.dto.response.ConsultationMessageResponse;
 import fit.iuh.se.hschat.dto.response.ConsultationParticipantResponse;
 import fit.iuh.se.hschat.dto.response.ConsultationRequestResponse;
 import fit.iuh.se.hschat.dto.response.ConsultationSessionResponse;
+import fit.iuh.se.hschat.dto.response.CareServicePackageResponse;
+import fit.iuh.se.hschat.dto.response.DoctorCareProfileResponse;
+import fit.iuh.se.hschat.entity.CareServicePackage;
 import fit.iuh.se.hschat.entity.ConsultationMessage;
 import fit.iuh.se.hschat.entity.ConsultationParticipant;
 import fit.iuh.se.hschat.entity.ConsultationRequest;
 import fit.iuh.se.hschat.entity.ConsultationSession;
+import fit.iuh.se.hschat.entity.DoctorCareProfile;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -18,6 +22,10 @@ public interface ConsultationMapper {
     ConsultationSessionResponse toSessionResponse(ConsultationSession session);
 
     ConsultationRequestResponse toRequestResponse(ConsultationRequest request);
+
+    CareServicePackageResponse toCareServicePackageResponse(CareServicePackage careServicePackage);
+
+    DoctorCareProfileResponse toDoctorCareProfileResponse(DoctorCareProfile doctorCareProfile);
 
     ConsultationMessageResponse toMessageResponse(ConsultationMessage message);
 
