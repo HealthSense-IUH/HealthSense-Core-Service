f839c03d8995a627788e6adabaae38d5bfb1e390
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-08-18 21:29:06 +0700

    chore: setup CI/CD pipeline, application configuration, and base test suite

--- hs-application/src/test/java/fit/iuh/se/hsapplication/HsApplicationTests.java
+++ hs-application/src/test/java/fit/iuh/se/hsapplication/HsApplicationTests.java
@@ -2,8 +2,10 @@ package fit.iuh.se.hsapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class HsApplicationTests {
 
     @Test
